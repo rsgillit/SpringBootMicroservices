fe370ec9e2d8bb5f0fa7dd8e11968c5dd22cbb7b
rsgillit <rsgillit@gmail.com>
2020-01-11 13:51:23 -0600

    fixes test for mapper

--- mss-brewery-client/src/main/java/com/gill/mssbreweryclient/web/model/BeerDto.java
+++ mss-brewery-client/src/main/java/com/gill/mssbreweryclient/web/model/BeerDto.java
@@ -12,8 +12,13 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+
+
     private UUID id;
+
     private String beerName;
+
     private String beerStyle;
+
     private Long upc;
 }

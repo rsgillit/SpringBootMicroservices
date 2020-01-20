b4eeccbb9ea267fca8f4b41726fca85836f87706
Rashpal Gill <rsgillit@gmail.com>
2020-01-20 17:23:37 -0600

    jms project

---
+++ sfg-jms/src/test/java/com/gill/sfgjms/SfgJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gill.sfgjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
